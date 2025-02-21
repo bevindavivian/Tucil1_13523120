76d21111b5874e774b67a99268210638fa30eba0
Bevinda Vivian <19623228@std.stei.itb.ac.id>
2025-02-21 21:06:55 +0700

    fix: blocks management fixed + array converter fixed

--- src/Board_Bev.java
+++ src/Board_Bev.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Board_Bev {
-    long count_bev14;
     int baris, kolom;
     char[][] board;
     ArrayList<Block> blocks;
@@ -10,22 +9,37 @@ public class Board_Bev {
         this.baris = baris;
         this.kolom = kolom;
         this.board = new char[baris][kolom];
-        this.count_bev14 = 0;
         this.blocks = new ArrayList<>();
 
         for (int i = 0; i < baris; i++) {
             for (int j = 0; j < kolom; j++) {
-                board[i][j] = '.'; 
+                board[i][j] = '.';
             }
         }
     }
 
-    public void addBlock(char huruf, String[] shapeStr) {
-        blocks.add(new Block(huruf, shapeStr));
+    public void tambahBlock(char huruf, char[][] shape) {
+        boolean[][] blockShape = convertArray(shape);
+        blocks.add(new Block(huruf, blockShape));
+    }
+
+    private boolean[][] convertArray(char[][] shape) {
+        int panjang = shape.length;
+        int lebar = shape[0].length;
+        boolean[][] booleanShape = new boolean[panjang][lebar];
+        
+        for (int i = 0; i < panjang; i++) {
+            for (int j = 0; j < lebar; j++) {
+                booleanShape[i][j] = (shape[i][j] != '.'); 
+            }
+        }
+        return booleanShape;
     }
 
     boolean cekAvail(Block block, int baris, int kolom) {
-        if (baris + block.height > this.baris || kolom + block.lebar > this.kolom) return false;
+        if (baris + block.height > this.baris || kolom + block.lebar > this.kolom) {
+            return false;
+        }
 
         for (int i = 0; i < block.height; i++) {
             for (int j = 0; j < block.lebar; j++) {
@@ -57,71 +71,36 @@ public class Board_Bev {
         }
     }
 
-    void printBoard() {
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public char[][] getBoard() {
-        return board;
-    }
-
     public class Block {
         char huruf;
         boolean[][] bentuk;
         int height, lebar;
 
-        public Block(char huruf, String[] shapeStr) {
+        public Block(char huruf, boolean[][] shape) {
             this.huruf = huruf;
-            this.height = shapeStr.length;
-            this.lebar = 0;
-            for (String baris : shapeStr) {
-                this.lebar = Math.max(this.lebar, baris.length());
-            }
-
-            this.bentuk = new boolean[height][lebar];
-
-            for (int i = 0; i < height; i++) {
-                String baris = shapeStr[i];
-                for (int j = 0; j < lebar; j++) {
-                    if (j < baris.length()) {
-                        this.bentuk[i][j] = (baris.charAt(j) == huruf);
-                    } else {
-                        this.bentuk[i][j] = false;
-                    }
-                }
-            }
+            this.height = shape.length;
+            this.lebar = shape[0].length;
+            this.bentuk = shape;
         }
 
         public Block rotate() {
-            String[] rotated = new String[lebar];
-            StringBuilder[] sb = new StringBuilder[lebar];
-
-            for (int i = 0; i < lebar; i++) {
-                sb[i] = new StringBuilder();
-                for (int j = height - 1; j >= 0; j--) {
-                    sb[i].append(bentuk[j][i] ? huruf : '.');
+            boolean[][] rotatedShape = new boolean[lebar][height];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < lebar; j++) {
+                    rotatedShape[j][height - 1 - i] = bentuk[i][j];
                 }
-                rotated[i] = sb[i].toString();
             }
-
-            return new Block(huruf, rotated);
+            return new Block(huruf, rotatedShape);
         }
 
         public Block flip() {
-            String[] flipped = new String[height];
+            boolean[][] flippedShape = new boolean[height][lebar];
             for (int i = 0; i < height; i++) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = lebar - 1; j >= 0; j--) {
-                    sb.append(bentuk[i][j] ? huruf : '.');
+                for (int j = 0; j < lebar; j++) {
+                    flippedShape[i][lebar - 1 - j] = bentuk[i][j];
                 }
-                flipped[i] = sb.toString();
             }
-            return new Block(huruf, flipped);
+            return new Block(huruf, flippedShape);
         }
     }
 }
